420b08d24635ae94cfd97a54cafe7dc4f8503276
Sanjay <sanjayhcu01@gmail.com>
2018-05-23 17:26:02 +0530

    Update userEventScript.js

--- NetSuiteScript/userEventScript.js
+++ NetSuiteScript/userEventScript.js
@@ -8,13 +8,11 @@ function beforeSubmit(type)
 {
    var newId = nlapiGetRecordId();
    var newType = nlapiGetRecordType();
-   console.log("Hii this is user event log")
    nlapiLogExecution('DEBUG','<Before Submit Script> type:'+type+', RecordType: '+newType+', Id:'+newId);
 }
 function afterSubmit(type)
 {
    var newId = nlapiGetRecordId();
    var newType = nlapiGetRecordType();
-   console.log("Hii this is user event log")
    nlapiLogExecution('DEBUG','<After Submit Script> type:'+type+', RecordType: '+newType+', Id:'+newId);
 }
